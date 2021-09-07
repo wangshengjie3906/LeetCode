8d94f17d8ddd7717551b04c75bd9180144c47086
wangshengjie3906 <2567916302@qq.com>
2021-09-07 23:09:03 +0800

    2021-09-07

---
+++ 0144.二叉树的前序遍历/0144-二叉树的前序遍历.c
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+
+void order(struct TreeNode *root, int *res, int *returnSize){
+    if(root == NULL)
+    {
+        return;
+    }
+
+    res[(*returnSize)++] = root->val;
+    order(root->left, res, returnSize);
+    order(root->right, res, returnSize);
+}
+
+int* preorderTraversal(struct TreeNode* root, int* returnSize){
+    // if(root == NULL)//这里不要进行空指针判断，否则反而是错的
+    // {
+    //     return NULL;
+    // }
+
+    int *res = malloc(sizeof(int) * 2048); //注意申请内存长度
+    *returnSize = 0;
+
+    order(root, res, returnSize);
+
+    return res;
+}
