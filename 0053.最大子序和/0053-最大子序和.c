68405ef9d6c344b60455c486e5b7367b316f529c
wangshengjie3906 <2567916302@qq.com>
2021-09-13 22:00:50 +0800

    2021-09-13

---
+++ 0053.最大子序和/0053-最大子序和.c
@@ -0,0 +1,49 @@
+int maxSubArray(int* nums, int numsSize){
+    /*暴力解法，运行超时*/
+    /*
+    if(numsSize == 1)
+    {
+        return nums[0];
+    }
+
+    int temp = 0;
+    int sum = nums[0];
+
+    for(int i = 0; i < numsSize; i++)
+    {
+        for(int j = 0; j < numsSize - i; j++)
+        {
+            temp =0;
+            for(int k = 0; k <= i; k++)
+            {
+                temp += nums[j + k];
+            }
+
+            if(temp > sum)
+            {
+                sum = temp;
+            }
+        }
+    }
+
+    return sum;
+    */
+
+    /*动态规划解法*/
+
+    if(numsSize == 1)
+    {
+        return nums[0];
+    }
+
+    int sum = nums[0];
+    int temp = 0;
+
+    for(int i = 0; i < numsSize; i++)
+    {
+        temp = (temp + nums[i]) > nums[i] ? (temp + nums[i]) : nums[i];
+        sum = temp > sum ? temp : sum;
+    }
+
+    return sum;
+}
