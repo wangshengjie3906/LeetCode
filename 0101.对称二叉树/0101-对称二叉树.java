25d3c2ef58e7e4286531bb5a5e9809c8b2d56e1c
wangshengjie3906 <2567916302@qq.com>
2021-10-03 22:49:50 +0800

    2021-10-03

---
+++ 0101.对称二叉树/0101-对称二叉树.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null)
+        {
+            return true;
+        }
+
+        return cmpChild (root.left, root.right);
+    }
+
+    public boolean cmpChild(TreeNode p, TreeNode q){
+        if(p == null && q == null)
+        {
+            return true;
+        }
+
+        if(p == null || q == null)
+        {
+            return false;
+        }
+
+        if(p.val != q.val)
+        {
+            return false;
+        }
+
+        return (cmpChild(p.left, q.right) && cmpChild (p.right, q.left));
+    }
+
+}
