ec41f4b85e48a360c3d8b83a651c5f95b9c25f3e
wangshengjie3906 <2567916302@qq.com>
2021-10-12 22:48:08 +0800

    2021-10-12

---
+++ 0724.寻找数组的中心下标/0724-寻找数组的中心下标.c
@@ -0,0 +1,17 @@
+int pivotIndex(int* nums, int numsSize) {
+    int total = 0;
+    //求和
+    for (int i = 0; i < numsSize; ++i) {
+        total += nums[i];
+    }
+
+    //从头开始，计算和为总的一半时
+    int sum = 0;
+    for (int i = 0; i < numsSize; ++i) {
+        if (2 * sum + nums[i] == total) {
+            return i;
+        }
+        sum += nums[i];
+    }
+    return -1;
+}
