0833482b75eb7850cc2f98bd92e2613bed007696
wangshengjie3906 <2567916302@qq.com>
2021-10-28 22:31:02 +0800

    2021-10-28

---
+++ 0344.反转字符串/0344-反转字符串.c
@@ -0,0 +1,15 @@
+void reverseString(char* s, int sSize){
+    if(sSize == 1)
+    {
+        return;
+    }
+    
+    char temp;
+    for(int i = 0; i < sSize / 2; i++)
+    {
+        temp = s[i];
+        s[i] = s[sSize - 1 - i];
+        s[sSize - 1 - i] = temp;
+    }
+    return;
+}
