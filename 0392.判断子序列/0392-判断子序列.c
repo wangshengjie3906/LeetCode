d0809555ff074757dee42e9cc19c652eacf67194
wangshengjie3906 <2567916302@qq.com>
2021-10-29 20:48:40 +0800

    2021-10-29

---
+++ 0392.判断子序列/0392-判断子序列.c
@@ -0,0 +1,11 @@
+bool isSubsequence(char* s, char* t) {
+    int n = strlen(s), m = strlen(t);
+    int i = 0, j = 0;
+    while (i < n && j < m) {
+        if (s[i] == t[j]) {
+            i++;
+        }
+        j++;
+    }
+    return i == n;
+}
