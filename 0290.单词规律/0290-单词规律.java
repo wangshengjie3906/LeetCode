c5ce5030f395718aa518190d8853c4303795d608
wangshengjie3906 <2567916302@qq.com>
2021-10-30 21:06:51 +0800

    2021-10-30

---
+++ 0290.单词规律/0290-单词规律.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean wordPattern(String pattern, String str) {
+        String[] words = str.split(" ");
+        if (words.length != pattern.length()) {
+            return false;
+        }
+        Map<Object, Integer> map = new HashMap<>();
+        for (Integer i = 0; i < words.length; i++) {
+            if (map.put(pattern.charAt(i), i) != map.put(words[i], i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
