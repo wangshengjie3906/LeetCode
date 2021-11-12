a2761b6b3afa30fcade8e7e4eae0ba6c23a30a07
wangshengjie3906 <2567916302@qq.com>
2021-11-12 20:46:10 +0800

    2021-11-12

---
+++ 0461.汉明距离/0461-汉明距离.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int s = x ^ y;
+        int ret = 0;
+        while (s != 0)
+        {
+            s &= s - 1;
+            ret++;
+        }
+        return ret;
+    }
+};
