b475f7e04e6de97d2b45de5bf42402e979a23a91
wangshengjie3906 <2567916302@qq.com>
2021-11-14 21:51:41 +0800

    2021-11-14

--- 0191.位1的个数/0191-位1的个数.java
+++ 0191.位1的个数/0191-位1的个数.java
@@ -1,14 +1,24 @@
 public class Solution {
     // you need to treat n as an unsigned value
     public int hammingWeight(int n) {
+        /*
         int flag = 1;
         int count = 0;
         for(int i = 0;i < 32; i++){
-            if((n&flag) != 0){
+            if((n & flag) != 0){
                 count++;
             }
             flag = flag<<1;
         }
         return count;
+        */
+
+        int num = 0;
+        while (n != 0)
+        {
+            n = n & (n - 1);
+            num++;
+        }
+        return num;
     }
 }
