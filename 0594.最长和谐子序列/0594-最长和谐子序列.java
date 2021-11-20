6c22c57cbb3bf4d9f1d6d8ef66645a1633ea750a
wangshengjie3906 <2567916302@qq.com>
2021-11-20 22:46:24 +0800

    2021-11-20

---
+++ 0594.最长和谐子序列/0594-最长和谐子序列.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findLHS(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length, ans = 0;
+        for (int i = 0, j = 0; j < n; j++)
+        {
+            while (i < j && nums[j] - nums[i] > 1)
+            {
+                i++;
+            }
+            if (nums[j] - nums[i] == 1)
+            {
+                ans = Math.max(ans, j - i + 1);
+            }
+        }
+        return ans;
+    }
+}
