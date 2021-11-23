d2df9be8d6b2762f2c6ff7ff18e587f4e8e0552e
wangshengjie3906 <2567916302@qq.com>
2021-11-23 22:54:45 +0800

    2021-11-23

---
+++ 0397.整数替换/0397-整数替换.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int integerReplacement(int n) {
+        long temp = n;
+        int count = 0;
+
+        while (temp != 1)
+        {
+            if ((temp & 3) == 3 && temp != 3)
+            {
+                temp++;
+            }
+            else if ((temp & 1) == 1)
+            {
+                temp--;
+            }
+            else
+            {
+                temp >>= 1;
+            }
+
+            count++;
+        }
+
+        return count;
+    }
+};
